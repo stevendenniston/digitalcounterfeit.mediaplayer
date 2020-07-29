1d402dfa2e802490fa675d1bafe5736d4eb22018
Steven Denniston <66822069+stevendenniston@users.noreply.github.com>
2020-07-29 00:21:27 -0600

    the beginnings...

---
+++ src/digitalcounterfeit.mediaplayer.api/Controllers/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace digitalcounterfeit.mediaplayer.api.Controllers
+{
+    [ApiController]
+    [Route("api/audio")]
+    public class AudioController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AudioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("track")]
+        public async Task<IActionResult> GetAudioTrackAsync()
+        {
+            await Task.Yield();
+
+            var fileStream = new FileStream(_configuration.GetValue<string>("AudioFilename"), FileMode.Open);
+
+            return new FileStreamResult(fileStream, "audio/mpeg");
+        }
+    }
+}
