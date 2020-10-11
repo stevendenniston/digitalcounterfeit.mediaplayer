819e6e795cec9048d0ccb4bc4bce3cc71d3ef05c
Steven Denniston <66822069+stevendenniston@users.noreply.github.com>
2020-10-11 00:37:17 -0600

    man i suck at front end, hop things get easier in the future...

--- src/digitalcounterfeit.mediaplayer.client/src/app/app-routing.module.ts
+++ src/digitalcounterfeit.mediaplayer.client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { PlayerComponent } from "./components/player/player.component";
+import { ArtistComponent } from './components/artist/artist.component';
 import { SigninCallbackComponent } from "./components/signin-callback/signin-callback.component";
 import { SignoutCallbackComponent } from "./components/signout-callback/signout-callback.component";
 
 const routes: Routes = [
-  { path: "", component: PlayerComponent },
+  { path: "", component: ArtistComponent },
   { path: "signin-callback", component: SigninCallbackComponent},
   { path: "signout-callback", component: SignoutCallbackComponent}
 ];
